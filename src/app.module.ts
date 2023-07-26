5ad5e9e27ad5228bf3b95345199bb6b71acecff5
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-07-26 14:54:54 +0300

    feat: Добавить подключение к базе данных

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDbConfig } from './config/db.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getDbConfig,
+    }),
+  ],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
