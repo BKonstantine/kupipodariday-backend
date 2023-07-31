3c1cd94ba339c64f316b8f015cfd86b28d0be06d
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-07-31 12:39:54 +0300

    feat: Добавить сущность wish для db

--- src/wishes/entities/wish.entity.ts
+++ src/wishes/entities/wish.entity.ts
@@ -1,1 +1,51 @@
-export class Wish {}
+import { Entity, Column } from 'typeorm';
+import {
+  IsArray,
+  IsInt,
+  IsNumber,
+  IsString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+import { BaseEntity } from 'src/entities/base.entity';
+
+@Entity()
+export class Wish extends BaseEntity {
+  @Column()
+  @Length(1, 250)
+  @IsString()
+  name: string;
+
+  @Column()
+  @IsUrl()
+  link: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  @IsNumber()
+  price: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  @IsNumber()
+  raised: number;
+
+  @Column({ type: 'integer', default: 0 })
+  @IsInt()
+  copied: number;
+
+  @Column()
+  @Length(1, 1024)
+  @IsString()
+  description: string;
+
+  @Column()
+  @IsString()
+  owner: string;
+
+  @Column()
+  @IsArray()
+  offers: [];
+}
