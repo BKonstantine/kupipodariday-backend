273253202fdfb2f642a916f7f6d525835377ba29
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-07-31 12:53:29 +0300

    feat: Добавить модель wishlist для db

--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,22 @@
-export class Wishlist {}
+import { Entity, Column } from 'typeorm';
+import { IsArray, IsString, IsUrl, Length } from 'class-validator';
+import { BaseEntity } from 'src/entities/base.entity';
+
+@Entity()
+export class Wishlist extends BaseEntity {
+  @Column()
+  @Length(1, 250)
+  @IsString()
+  name: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column()
+  owner: string;
+
+  @Column()
+  @IsArray()
+  items: [];
+}
