2699bade9f00a8f607b8e02085855585add15ded
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-07-31 19:02:41 +0300

    refactor: Изменить название методов HashService

--- src/hash/hash.service.ts
+++ src/hash/hash.service.ts
@@ -3,12 +3,12 @@ import { compare, genSalt, hash } from 'bcrypt';
 
 @Injectable()
 export class HashService {
-  async hash(password: string): Promise<string> {
+  async getHash(password: string): Promise<string> {
     const salt = await genSalt(10);
     return await hash(password, salt);
   }
 
-  async compare(password: string, hash: string): Promise<boolean> {
+  async comparePassword(password: string, hash: string): Promise<boolean> {
     return await compare(password, hash);
   }
 }
