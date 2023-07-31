9ce2b12cddf4bd7cdd3a5149a86e7292f6a7cc91
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-07-31 12:58:39 +0300

    feat: Добавить модель Offer для db

--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,1 +1,20 @@
-export class Offer {}
+import { Entity, Column } from 'typeorm';
+import { IsBoolean, IsNumber } from 'class-validator';
+import { BaseEntity } from 'src/entities/base.entity';
+
+@Entity()
+export class Offer extends BaseEntity {
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  @IsNumber()
+  amount: number;
+
+  @Column()
+  @IsBoolean()
+  hidden: string;
+
+  @Column()
+  user: string;
+
+  @Column()
+  item: string;
+}
