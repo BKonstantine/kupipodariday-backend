38321c05965e64cb27e2d6937c5a84de98aa3514
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-07-31 12:16:13 +0300

    feat: Добавить модель User для db

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,38 @@
-export class User {}
+import { Entity, Column } from 'typeorm';
+import { IsString, IsEmail, IsUrl, IsArray } from 'class-validator';
+import { BaseEntity } from 'src/entities/base.entity';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column({ unique: true })
+  @IsString()
+  username: string;
+
+  @Column({ default: 'Пока ничего не рассказал о себе' })
+  @IsString()
+  about: string;
+
+  @Column({ default: 'https://i.pravatar.cc/300' })
+  @IsUrl()
+  avatar: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column()
+  @IsString()
+  password: string;
+
+  @Column()
+  @IsArray()
+  wishes: [];
+
+  @Column()
+  @IsArray()
+  offers: [];
+
+  @Column()
+  @IsArray()
+  wishlists: [];
+}
