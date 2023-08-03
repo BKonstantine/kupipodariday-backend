8f6fbd67ef465f68b9f9f1eb392dd929edeef98b
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-08-03 18:46:28 +0300

    feat: Убрать закоментированный код

--- src/hash/hash.service.ts
+++ src/hash/hash.service.ts
@@ -19,14 +19,6 @@ export class HashService {
       password: hashPassword,
     };
   }
-  /* async getUserWithHash(createUserDto: CreateUserDto): Promise<CreateUserDto> {
-    const { password, ...rest } = createUserDto;
-    const hashPassword = await this.getHash(password);
-    return {
-      ...rest,
-      password: hashPassword,
-    };
-  } */
 
   async comparePassword(password: string, hash: string): Promise<boolean> {
     return await compare(password, hash);
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ServerExceptionFilter } from './filter/server-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  //app.useGlobalFilters(new ServerExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
 }
